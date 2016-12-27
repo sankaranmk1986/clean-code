4dcbf4a76e34ec9350b20f02fd264b1e38106d6d
unknown <XF76@INLTD09921.global.tesco.org>
2016-12-27 11:56:18 +0530

    GREEN - Returning count as 1 for all single words

--- src/main/java/org/cleancode/katas/WordCounter.java
+++ src/main/java/org/cleancode/katas/WordCounter.java
@@ -5,8 +5,13 @@ import java.util.Map;
 
 public class WordCounter {
 
-	public Map<String, Integer> countWords(String word) {		
-		return new HashMap<String, Integer>();
+	public Map<String, Integer> countWords(String word) {	
+		if(word.isEmpty()){
+			return new HashMap<String, Integer>();
+		}
+		Map<String, Integer> wordcountMap = new HashMap<>();
+		wordcountMap.put(word, 1);
+		return wordcountMap;
 	}
 
 }
