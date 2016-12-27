50504bd88f9a54e2f35cc15afcf812729b4652e8
unknown <XF76@INLTD09921.global.tesco.org>
2016-12-27 12:11:18 +0530

    RED - should return count 1 for 2 different words seperated by space

--- src/test/java/org/cleancode/katas/WordCounterShould.java
+++ src/test/java/org/cleancode/katas/WordCounterShould.java
@@ -38,5 +38,13 @@ public class WordCounterShould {
 		assertMapSize(wordcountMap, 1);
 		assertThat(wordcountMap.get("word"), is(1));
 	}
+	
+	@Test
+	public void return_count_1_for_2_different_words_with_space(){
+		Map<String, Integer> wordcountMap = wordCounter.countWords("word count");
+		assertMapSize(wordcountMap, 2);
+		assertThat(wordcountMap.get("word"), is(1));
+		assertThat(wordcountMap.get("count"), is(1));
+	}
 
 }
