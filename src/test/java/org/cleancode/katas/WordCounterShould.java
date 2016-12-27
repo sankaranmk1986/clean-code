5468153955a1b1982ea236da7cda56d0ca6076ac
unknown <XF76@INLTD09921.global.tesco.org>
2016-12-27 11:07:53 +0530

    RED - should be able to create WordCounter Object

---
+++ src/test/java/org/cleancode/katas/WordCounterShould.java
@@ -0,0 +1,16 @@
+package org.cleancode.katas;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class WordCounterShould {
+	
+	@Test
+	public void able_to_create_wordcounter_object(){
+		WordCounter wordCounter = new WordCounter();
+		assertNotNull(wordCounter);
+	}
+
+}
