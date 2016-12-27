bd59b170fdf17345190280fff26b7e0cfd9ce4fd
unknown <XF76@INLTD09921.global.tesco.org>
2016-12-27 11:44:21 +0530

    RED - Return empty map for empty string

--- src/test/java/org/cleancode/katas/WordCounterShould.java
+++ src/test/java/org/cleancode/katas/WordCounterShould.java
@@ -1,7 +1,13 @@
 package org.cleancode.katas;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
+import java.util.HashMap;
+import java.util.Map;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 public class WordCounterShould {
@@ -10,6 +16,13 @@ public class WordCounterShould {
 	public void able_to_create_wordcounter_object(){
 		WordCounter wordCounter = new WordCounter();
 		assertNotNull(wordCounter);
+	}
+	
+	@Test
+	public void return_emptymap_for_empty_string(){
+		WordCounter wordCounter = new WordCounter();
+		Map<String, Integer> wordCountMap = wordCounter.countWords("");
+		assertThat(wordCountMap.size(), is(0));
 	}
 
 }
