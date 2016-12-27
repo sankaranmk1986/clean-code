4f4b6404ae904ca736ec5d709148b5ec252d184d
unknown <XF76@INLTD09921.global.tesco.org>
2016-12-27 11:50:27 +0530

    REFACTOR - Object creation to setup mathod

--- src/test/java/org/cleancode/katas/WordCounterShould.java
+++ src/test/java/org/cleancode/katas/WordCounterShould.java
@@ -6,19 +6,25 @@ import static org.junit.Assert.assertThat;
 
 import java.util.Map;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class WordCounterShould {
 	
-	@Test
-	public void able_to_create_wordcounter_object(){
-		WordCounter wordCounter = new WordCounter();
-		assertNotNull(wordCounter);
+	private WordCounter wordCounter;
+	
+	@Before
+	public void setup() {
+		wordCounter = new WordCounter();
 	}
 	
 	@Test
+	public void able_to_create_wordcounter_object(){
+		assertNotNull(wordCounter);
+	}	
+	
+	@Test
 	public void return_emptymap_for_empty_string(){
-		WordCounter wordCounter = new WordCounter();
 		Map<String, Integer> wordCountMap = wordCounter.countWords("");
 		assertThat(wordCountMap.size(), is(0));
 	}
