e827cc9c8358ac55f9475dfc2f74a1f7f77a97aa
unknown <XF76@INLTD09921.global.tesco.org>
2016-12-29 05:04:03 +0530

    REFACTOR - Parameterized test cases

--- src/test/java/org/cleancode/katas/NumberToWordsShould.java
+++ src/test/java/org/cleancode/katas/NumberToWordsShould.java
@@ -1,14 +1,53 @@
 package org.cleancode.katas;
 
+import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.util.Collection;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(value=Parameterized.class)
 public class NumberToWordsShould {
 	
 	private NumberToWords numberToWords;
+	
+	private int number;
+	private String numberInWords;
+	
+	public NumberToWordsShould(int number, String numberInWords){
+		this.number=number;
+		this.numberInWords=numberInWords;
+	}
+	
+	 @Parameters(name = "return_{1}_for_number_{0}()")
+	 public static Collection<Object[]> data() {
+	    return asList(new Object[][] {
+	        {1, "ONE"},
+	        {2, "TWO"},
+	        {3, "THREE"},
+	        {10, "TEN"},
+	        {19, "NINETEEN"},
+	        {20, "TWENTY"},
+	        {21, "TWENTY ONE"},
+	        {30, "THIRTY"},
+	        {99, "NINETY NINE"},
+	        {100, "ONE HUNDRED"},
+	        {101, "ONE HUNDRED AND ONE"},
+	        {341, "THREE HUNDRED AND FOURTY ONE"},
+	        {999, "NINE HUNDRED AND NINETY NINE"},
+	        {1000, "ONE THOUSAND"},
+	        {1001, "ONE THOUSAND ONE"},
+	        {2657, "TWO THOUSAND SIX HUNDRED AND FIFTY SEVEN"},
+	        {8990, "EIGHT THOUSAND NINE HUNDRED AND NINETY"},
+	        {9999, "NINE THOUSAND NINE HUNDRED AND NINETY NINE"}
+	    });
+	  }
 	
 	@Before
 	public void setup(){
@@ -16,72 +55,7 @@ public class NumberToWordsShould {
 	}
 	
 	@Test
-	public void return_one_for_number_1(){
-		assertThat(numberToWords.convertToWords(1),is("ONE"));
-	}
-	
-	@Test
-	public void return_two_for_number_2(){
-		assertThat(numberToWords.convertToWords(2),is("TWO"));
-	}
-	
-	@Test
-	public void return_three_for_number_3(){
-		assertThat(numberToWords.convertToWords(3),is("THREE"));
-	}
-	
-	@Test
-	public void return_nineteen_for_number_19(){
-		assertThat(numberToWords.convertToWords(19),is("NINETEEN"));
-	}
-	
-	@Test
-	public void return_twentyone_for_number_21(){
-		assertThat(numberToWords.convertToWords(21),is("TWENTY ONE"));
-	}
-	
-	@Test
-	public void return_thirty_for_number_30(){
-		assertThat(numberToWords.convertToWords(30),is("THIRTY"));
-	}
-
-	@Test
-	public void return_ninetynine_for_number_99(){
-		assertThat(numberToWords.convertToWords(99),is("NINETY NINE"));
-	}
-	
-	@Test
-	public void return_onehundred_for_number_100(){
-		assertThat(numberToWords.convertToWords(100),is("ONE HUNDRED"));
-	}
-	
-	@Test
-	public void return_onehundredandone_for_number_101(){
-		assertThat(numberToWords.convertToWords(101),is("ONE HUNDRED AND ONE"));
-	}
-	
-	@Test
-	public void return_ninehundredandnintynine_for_number_999(){
-		assertThat(numberToWords.convertToWords(999),is("NINE HUNDRED AND NINETY NINE"));
-	}
-	
-	@Test
-	public void return_onethousand_for_number_1000(){
-		assertThat(numberToWords.convertToWords(1000),is("ONE THOUSAND"));
-	}
-	
-	@Test
-	public void return_onethousandone_for_number_1001(){
-		assertThat(numberToWords.convertToWords(1001),is("ONE THOUSAND ONE"));
-	}
-	
-	@Test
-	public void return_onethousand_onehundred_and_one_for_number_1101(){
-		assertThat(numberToWords.convertToWords(1101),is("ONE THOUSAND ONE HUNDRED AND ONE"));
-	}
-	
-	@Test
-	public void return_ninethousand_ninehundred_and_ninety_nine_for_number_9999(){
-		assertThat(numberToWords.convertToWords(9999),is("NINE THOUSAND NINE HUNDRED AND NINETY NINE"));
+	public void return_word_for_given_number(){
+		assertThat(numberToWords.convertToWords(number), is(numberInWords));
 	}
 }
