42f3e48af42d82cb7e14fdff05ff47115f0f0942
unknown <XF76@INLTD09921.global.tesco.org>
2016-12-29 02:42:09 +0530

    GREEN - return HUNDRED for 100

--- src/main/java/org/cleancode/katas/NumberToWords.java
+++ src/main/java/org/cleancode/katas/NumberToWords.java
@@ -5,6 +5,9 @@ public class NumberToWords {
 	private TensProcessor numberProcessor = new TensProcessor();
 	
 	public String convertToWords(int number) {
+		if(number == 100){
+			return "ONE HUNDRED";
+		}
 		return numberProcessor.convertToWord(number);
 	}
 }
