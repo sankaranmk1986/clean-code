71f18a5d741fc22ea84bdfb2ec67fa72be25b96e
unknown <XF76@INLTD09921.global.tesco.org>
2016-12-29 01:53:02 +0530

    GREEN - append twenty if word is greater than 20

--- src/main/java/org/cleancode/katas/NumberToWords.java
+++ src/main/java/org/cleancode/katas/NumberToWords.java
@@ -5,6 +5,14 @@ import static java.util.stream.Stream.of;
 public class NumberToWords {
 
 	public String convertToWords(int number) {
+		StringBuilder wordBuilder = new StringBuilder();
+		if(number >= 20){
+			wordBuilder.append("TWENTY ");
+		}
+		return wordBuilder.append(convertOnesToWord(number%20)).toString();
+	}
+	
+	private String convertOnesToWord(int number){
 		return of(ONES.values())
 				.filter(ones -> ones.getNumber() ==number)
 				.findFirst()
