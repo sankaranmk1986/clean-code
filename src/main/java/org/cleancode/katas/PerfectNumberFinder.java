7513c99450d2ae4e5365baaeeb4428c531e1e1d4
unknown <XF76@INLTD09921.global.tesco.org>
2016-12-28 11:09:47 +0530

    GREEN - added, 28 to the if condition

--- src/main/java/org/cleancode/katas/PerfectNumberFinder.java
+++ src/main/java/org/cleancode/katas/PerfectNumberFinder.java
@@ -3,7 +3,7 @@ package org.cleancode.katas;
 public class PerfectNumberFinder {
 
 	public boolean isPerfectNumber(int number) {
-		if(number == 6){
+		if(number == 6 || number == 28){
 			return true;
 		}
 		return false;
