db25fc55215173ac6fd02055f60984a88e24da8d
unknown <XF76@INLTD09921.global.tesco.org>
2016-12-29 02:48:40 +0530

    GREEN - one hundred and one for 101

--- src/main/java/org/cleancode/katas/NumberToWords.java
+++ src/main/java/org/cleancode/katas/NumberToWords.java
@@ -5,9 +5,14 @@ public class NumberToWords {
 	private TensProcessor numberProcessor = new TensProcessor();
 	
 	public String convertToWords(int number) {
-		if(number == 100){
-			return "ONE HUNDRED";
+		StringBuilder wordBuilder = new StringBuilder();
+		if(number >= 100){
+			wordBuilder.append("ONE HUNDRED");
+			number=number%100;
+			if(number > 0){
+				wordBuilder.append(" AND ");
+			}
 		}
-		return numberProcessor.convertToWord(number);
+		return wordBuilder.append(numberProcessor.convertToWord(number)).toString();
 	}
 }
