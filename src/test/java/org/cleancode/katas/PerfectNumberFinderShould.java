cc7fb166f6ab8cc6bcc83b379f739b13638c4ef4
unknown <XF76@INLTD09921.global.tesco.org>
2016-12-28 10:04:13 +0530

    GREEN - created PerfectNumberFinder class

--- src/test/java/org/cleancode/katas/PerfectNumberFinderShould.java
+++ src/test/java/org/cleancode/katas/PerfectNumberFinderShould.java
@@ -2,14 +2,13 @@ package org.cleancode.katas;
 
 import static org.junit.Assert.assertNotNull;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 public class PerfectNumberFinderShould {
 
 	@Test
 	public void able_to_create_PerfectNumberFinder(){
-		PefectNumberFinder perfectNumberFinder = new PerfectNumberFinder();
+		PerfectNumberFinder perfectNumberFinder = new PerfectNumberFinder();
 		assertNotNull(perfectNumberFinder);
 	}
 	
