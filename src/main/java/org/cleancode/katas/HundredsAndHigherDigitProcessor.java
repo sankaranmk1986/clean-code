c31adab181d7294c52b8dd0438a4e50d28f3447a
unknown <XF76@INLTD09921.global.tesco.org>
2016-12-29 03:48:38 +0530

    REFACTOR - Chanaged HundredProcesseor to HigherDigitProcessor

---
+++ src/main/java/org/cleancode/katas/HundredsAndHigherDigitProcessor.java
@@ -0,0 +1,42 @@
+package org.cleancode.katas;
+
+public class HundredsAndHigherDigitProcessor implements NumberProcessor {
+	
+	private final String HUNDRED="HUNDRED";
+	
+	private final String THOUSAND="THOUSAND";
+	
+	private OnesProcessor onesProcessor = new OnesProcessor();
+	
+	private TensProcessor tensProcessor = new TensProcessor();
+	
+	@Override
+	public String convertToWord(int number) {
+		boolean thousandFound=false;
+		StringBuilder wordBuilder = new StringBuilder();
+		if(number >= 1000){	
+			wordBuilder.append(convertFirstDigitToWords(number/1000, THOUSAND));
+			number=number%1000;
+			thousandFound=true;
+		}	
+		convertHundredsToWord(number, thousandFound, wordBuilder);
+		return wordBuilder.append(tensProcessor.convertToWord(number)).toString();
+	}
+
+	private void convertHundredsToWord(int number, boolean thousandFound, StringBuilder wordBuilder) {
+		if(number >= 100){
+			if(thousandFound){
+				wordBuilder.append(" ");
+			}
+			wordBuilder.append(convertFirstDigitToWords(number/100, HUNDRED));
+		}
+	}
+	
+	private String convertFirstDigitToWords(int number, String wordToAppend){
+		return new StringBuilder().append(onesProcessor.convertToWord(number))
+					.append(" ")
+					.append(wordToAppend).toString();
+		
+	}
+
+}
--- src/main/java/org/cleancode/katas/NumberToWords.java
+++ src/main/java/org/cleancode/katas/NumberToWords.java
@@ -2,12 +2,9 @@ package org.cleancode.katas;
 
 public class NumberToWords {
 	
-	private NumberProcessor numberProcessor = new HundredProcessor();
+	private NumberProcessor numberProcessor = new HundredsAndHigherDigitProcessor();
 	
 	public String convertToWords(int number) {	
-		if(number == 1000){
-			return "ONE THOUSAND";
-		}
 		return numberProcessor.convertToWord(number);
 	}
 }
