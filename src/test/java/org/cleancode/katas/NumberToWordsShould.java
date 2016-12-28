07b604c2dd7c16899547babae47ba7a732329779
unknown <XF76@INLTD09921.global.tesco.org>
2016-12-29 01:12:04 +0530

    REFACTOR - removing duplicate code to setup method

--- src/test/java/org/cleancode/katas/NumberToWordsShould.java
+++ src/test/java/org/cleancode/katas/NumberToWordsShould.java
@@ -3,18 +3,26 @@ package org.cleancode.katas;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class NumberToWordsShould {
 	
+	private NumberToWords numberToWords;
+	
+	@Before
+	public void setup(){
+		numberToWords = new NumberToWords();
+	}
+	
 	@Test
 	public void return_one_for_number_1(){
-		assertThat(new NumberToWords().convertToWords(1),is("ONE"));
+		assertThat(numberToWords.convertToWords(1),is("ONE"));
 	}
 	
 	@Test
 	public void return_two_for_number_1(){
-		assertThat(new NumberToWords().convertToWords(2),is("TWO"));
+		assertThat(numberToWords.convertToWords(2),is("TWO"));
 	}
 
 }
