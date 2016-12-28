54226e39dc8ac5f16fcafc2746395744f29d1960
unknown <XF76@INLTD09921.global.tesco.org>
2016-12-29 03:28:16 +0530

    GREEN - return 1000

--- src/main/java/org/cleancode/katas/NumberToWords.java
+++ src/main/java/org/cleancode/katas/NumberToWords.java
@@ -5,6 +5,9 @@ public class NumberToWords {
 	private NumberProcessor numberProcessor = new HundredProcessor();
 	
 	public String convertToWords(int number) {	
+		if(number == 1000){
+			return "ONE THOUSAND";
+		}
 		return numberProcessor.convertToWord(number);
 	}
 }
