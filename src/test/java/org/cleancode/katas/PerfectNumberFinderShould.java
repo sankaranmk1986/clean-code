260e34c98d8d607ae9c6796c4f22601c2056d7c1
unknown <XF76@INLTD09921.global.tesco.org>
2016-12-28 11:02:41 +0530

    REFACTOR - moved object creation to setup method

--- src/test/java/org/cleancode/katas/PerfectNumberFinderShould.java
+++ src/test/java/org/cleancode/katas/PerfectNumberFinderShould.java
@@ -3,19 +3,26 @@ package org.cleancode.katas;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class PerfectNumberFinderShould {
+	
+	private PerfectNumberFinder perfectNumberFinder;
+	
+	@Before
+	public void setup(){
+		perfectNumberFinder = new PerfectNumberFinder();
+	}
+	
 
 	@Test
 	public void able_to_create_PerfectNumberFinder(){
-		PerfectNumberFinder perfectNumberFinder = new PerfectNumberFinder();
 		assertNotNull(perfectNumberFinder);
 	}
 	
 	@Test
 	public void return_false_for_number_1(){
-		PerfectNumberFinder perfectNumberFinder = new PerfectNumberFinder();
 		assertFalse(perfectNumberFinder.isPerfectNumber(1));
 	}
 	
