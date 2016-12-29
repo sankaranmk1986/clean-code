8b8a2309e72b65d825b76f5d38da09378b5de5e1
unknown <XF76@INLTD09921.global.tesco.org>
2016-12-29 19:12:07 +0530

    REFACTOR - formatted for better readability, avoided creating duplicte builders

--- src/main/java/org/cleancode/katas/OnesProcessor.java
+++ src/main/java/org/cleancode/katas/OnesProcessor.java
@@ -5,23 +5,25 @@ import static java.util.stream.Stream.of;
 public class OnesProcessor implements NumberProcessor {
 
 	public String convertToWord(int number) {
-		boolean tensFound=false;
 		if (number >= 20) {
-			number = number % 10;		
+			number = number % 10;
 		}
-		return convertOnesToWord(number,tensFound);
+		return convertOnesToWord(number);
 	}
 
-	private String convertOnesToWord(int number, boolean tensFound) {
-		StringBuilder wordBuilder = new StringBuilder();
+	private String convertOnesToWord(int number) {
 		if (number > 0) {
-			wordBuilder.append(of(ONES.values())
-								.filter(ones -> ones.getNumber() == number)
-								.findFirst()
-								.get()
-								.name());
-		}	
-		return wordBuilder.toString();
+			return onesToWord(number);
+		}
+		return "";
+	}
+
+	private String onesToWord(int number) {
+		return of(ONES.values())//
+				.filter(ones -> ones.getNumber() == number)//
+				.findFirst()//
+				.get()//
+				.name();
 	}
 
 }
