e7cefd81393dac46cd5715ff5a42f6beda966141
unknown <XF76@INLTD09921.global.tesco.org>
2016-12-29 19:11:44 +0530

    REFACTOR - formatted for better readability, avoided creating duplicte builders

--- src/main/java/org/cleancode/katas/TensProcessor.java
+++ src/main/java/org/cleancode/katas/TensProcessor.java
@@ -3,30 +3,31 @@ package org.cleancode.katas;
 import static java.util.stream.Stream.of;
 
 public class TensProcessor implements NumberProcessor {
-	
+
 	private OnesProcessor onesProcessor = new OnesProcessor();
-	
-	public String convertToWord(int number){
+
+	public String convertToWord(int number) {
 		StringBuilder wordBuilder = new StringBuilder();
-		wordBuilder.append(convertTensToWord(number%100));
+		convertTensToWord(number % 100, wordBuilder);
 		return wordBuilder.append(onesProcessor.convertToWord(number)).toString();
-	}	
-	
-	private String convertTensToWord(int number){
-		StringBuilder wordBuilder = new StringBuilder();
-		if(number >=20){
-			int tenthDigit = number/10;
-			wordBuilder.append(of(TENS.values())
-					.filter(tens -> tens.getExponent() ==tenthDigit)
-					.findFirst()
-					.get()
-					.name());
-			if(number%10 > 0){
-				wordBuilder.append(" ");
-			}			
-		}
-		return wordBuilder.toString();
 	}
 
+	private void convertTensToWord(int number,StringBuilder wordBuilder) {
+		if (number >= 20) {
+			int tenthDigit = number / 10;
+			wordBuilder.append(tenthDigitToWord(tenthDigit));
+			if (number % 10 > 0) {
+				wordBuilder.append(" ");
+			}
+		}
+	}
+
+	private String tenthDigitToWord(int tenthDigit) {
+		return of(TENS.values())//
+				.filter(tens -> tens.getExponent() == tenthDigit)//
+				.findFirst()//
+				.get()//
+				.name();
+	}
 
 }
