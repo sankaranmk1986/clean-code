7c93e9a5f93c3b567e3a3ca902be0863f7e63353
unknown <XF76@INLTD09921.global.tesco.org>
2016-12-29 19:12:23 +0530

    REFACTOR - formatted for better readability, avoided creating duplicte builders

--- src/main/java/org/cleancode/katas/HundredsAndHigherDigitProcessor.java
+++ src/main/java/org/cleancode/katas/HundredsAndHigherDigitProcessor.java
@@ -1,52 +1,52 @@
 package org.cleancode.katas;
 
 public class HundredsAndHigherDigitProcessor implements NumberProcessor {
-	
-	private String AND_WITH_SPACE=" AND ";
-	
-	private final String HUNDRED="HUNDRED";
-	
-	private final String THOUSAND="THOUSAND";
-	
+
+	private String AND_WITH_SPACE = " AND ";
+
+	private final String HUNDRED = "HUNDRED";
+
+	private final String THOUSAND = "THOUSAND";
+
 	private OnesProcessor onesProcessor = new OnesProcessor();
-	
+
 	private TensProcessor tensProcessor = new TensProcessor();
-	
+
 	@Override
 	public String convertToWord(int number) {
-		boolean thousandFound=false;
+		boolean thousandFound = false;
 		StringBuilder wordBuilder = new StringBuilder();
-		if(number >= 1000){	
-			wordBuilder.append(convertFirstDigitToWords(number/1000, THOUSAND));
-			thousandFound=true;
-		}	
+		if (number >= 1000) {
+			convertFirstDigitToWords(number / 1000, THOUSAND, wordBuilder);
+			thousandFound = true;
+		}
 		convertHundredsToWord(number, thousandFound, wordBuilder);
 		return wordBuilder.append(tensProcessor.convertToWord(number)).toString();
 	}
 
 	private void convertHundredsToWord(int number, boolean thousandFound, StringBuilder wordBuilder) {
-		if(thousandFound){
-			number=number%1000;
-			if(number > 0){
+		if (thousandFound) {
+			number = number % 1000;
+			if (number > 0) {
 				wordBuilder.append(" ");
-			}			
+			}
 		}
-		if(number >= 100){			
-			wordBuilder.append(convertFirstDigitToWords(number/100, HUNDRED));
-			appendAndWithSpace(number,wordBuilder);
+		if (number >= 100) {
+			convertFirstDigitToWords(number / 100, HUNDRED,wordBuilder);
+			appendAndWithSpace(number, wordBuilder);
 		}
 	}
-	
-	private String convertFirstDigitToWords(int number, String wordToAppend){
-		return new StringBuilder().append(onesProcessor.convertToWord(number))
-					.append(" ")
-					.append(wordToAppend).toString();
-		
+
+	private void convertFirstDigitToWords(int number, String wordToAppend, StringBuilder wordBuilder) {
+		wordBuilder.append(onesProcessor.convertToWord(number))//
+				.append(" ")//
+				.append(wordToAppend);
+
 	}
-	
-	private void appendAndWithSpace(int number, StringBuilder wordBuilder) {	
-		int remainder = number%100;
-		if(remainder>0){		
+
+	private void appendAndWithSpace(int number, StringBuilder wordBuilder) {
+		int remainder = number % 100;
+		if (remainder > 0) {
 			wordBuilder.append(AND_WITH_SPACE);
 		}
 	}
