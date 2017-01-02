13752d330606577f6c697639aae35f115852d6e6
unknown <XF76@INLTD09921.global.tesco.org>
2017-01-03 04:25:55 +0530

    GREEN- Discounte price for 4 items

--- src/test/java/org/cleancode/katas/DiscountShould.java
+++ src/test/java/org/cleancode/katas/DiscountShould.java
@@ -1,16 +1,39 @@
 package org.cleancode.katas;
 
+import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import org.junit.Test;
+import java.util.Collection;
 
-public class DiscountShould {	
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+@RunWith(value=Parameterized.class)
+public class DiscountShould {
+	
+	private int count, price, discountedPrice,purchasedProdcutCount;
+	
+	public DiscountShould(int count, int price,int purchasedProdcutCount, int discountedPrice) {
+		this.count=count;
+		this.price=price;
+		this.purchasedProdcutCount=purchasedProdcutCount;
+		this.discountedPrice=discountedPrice;
+	}
+	
+	 @Parameters(name = "return_{1}_for_items_{0}()")
+	  public static Collection<Object[]> data() {
+	    return asList(new Object[][] {
+	        {3, 130,3, 130},
+	        {3, 130, 4,130}	        
+	      });
+	  }
 	
 	@Test
 	public void return_130_as_discounted_price_for_AAA(){
-		Discount discount = new Discount(3, 130);
-		assertThat(discount.calculateDiscountedPrice(3), is(130));
+		Discount discount = new Discount(count, price);
+		assertThat(discount.calculateDiscountedPrice(purchasedProdcutCount), is(discountedPrice));
 	}
 
 }
