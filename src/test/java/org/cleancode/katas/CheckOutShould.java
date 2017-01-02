171ce8788bad0bfc7ee19336c3c5dfa1653c6469
unknown <XF76@INLTD09921.global.tesco.org>
2017-01-03 01:12:03 +0530

    REFACTOR - Object creation to @Before method

--- src/test/java/org/cleancode/katas/CheckOutShould.java
+++ src/test/java/org/cleancode/katas/CheckOutShould.java
@@ -3,19 +3,25 @@ package org.cleancode.katas;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CheckOutShould {
 	
+	private CheckOut checkOut;
+	
+	@Before
+	public void setup(){
+		checkOut = new CheckOut();
+	}
+	
 	@Test
 	public void return_0_if_no_product_added(){
-		CheckOut checkOut = new CheckOut();
 		assertThat(checkOut.price(""), is(0));		
 	}
 	
 	@Test
 	public void return_50_if_A_product_added(){
-		CheckOut checkOut = new CheckOut();
 		assertThat(checkOut.price("A"), is(50));		
 	}
 
