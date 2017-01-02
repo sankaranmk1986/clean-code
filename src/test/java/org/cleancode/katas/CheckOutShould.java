9307199da6076492228a34de336ab7a91e2144ea
unknown <XF76@INLTD09921.global.tesco.org>
2017-01-03 01:59:14 +0530

    REFACTOR - parameterized tests

--- src/test/java/org/cleancode/katas/CheckOutShould.java
+++ src/test/java/org/cleancode/katas/CheckOutShould.java
@@ -1,17 +1,39 @@
 package org.cleancode.katas;
 
+import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
-
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+@RunWith(value=Parameterized.class)
 public class CheckOutShould {
 	
 	private CheckOut checkOut;
+	
+	private String items;
+	private int price;
+	
+	public CheckOutShould(String items, int price){
+		this.items=items;
+		this.price=price;
+	}
+	
+	 @Parameters(name = "return_{1}_for_items_{0}()")
+	  public static Collection<Object[]> data() {
+	    return asList(new Object[][] {
+	        {"A", 50},
+	        {"B", 30},
+	        {"AB", 80},
+	      });
+	  }
 	
 	@Before
 	public void setup(){
@@ -23,24 +45,8 @@ public class CheckOutShould {
 	}
 	
 	@Test
-	public void return_0_if_no_product_added(){
-		assertThat(checkOut.price(""), is(0));		
+	public void return_price_for_items_added(){
+		assertThat(checkOut.price(items), is(price));		
 	}
-	
-	@Test
-	public void return_50_if_A_product_added(){
-		assertThat(checkOut.price("A"), is(50));		
-	}
-	
-	@Test
-	public void return_30_if_B_product_added(){
-		assertThat(checkOut.price("B"), is(30));		
-	}
-	
-	@Test
-	public void return_80_if_AB_product_added(){
-		assertThat(checkOut.price("AB"), is(80));		
-	}
-	
 
 }
