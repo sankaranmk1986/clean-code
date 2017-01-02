1fb89042427c495a017a0bb82ab5235d9faa7c82
unknown <XF76@INLTD09921.global.tesco.org>
2017-01-03 01:13:49 +0530

    GREEN - return 30 if B is added

--- src/main/java/org/cleancode/katas/CheckOut.java
+++ src/main/java/org/cleancode/katas/CheckOut.java
@@ -5,6 +5,8 @@ public class CheckOut {
 	public int price(String products) {
 		if(products.equalsIgnoreCase("A")){
 			return 50;
+		}else if(products.equalsIgnoreCase("B")){
+			return 30;
 		}
 		return 0;
 	}
