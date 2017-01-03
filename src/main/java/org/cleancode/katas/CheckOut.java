0b339ab8d722063324f99b8e53269a4321c0f549
unknown <XF76@INLTD09921.global.tesco.org>
2017-01-03 11:53:42 +0530

    REFACTOR - removed duplicate codes

--- src/main/java/org/cleancode/katas/CheckOut.java
+++ src/main/java/org/cleancode/katas/CheckOut.java
@@ -1,20 +1,15 @@
 package org.cleancode.katas;
 
 import static java.util.Arrays.asList;
-import static java.util.stream.Collectors.toMap;
 
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class CheckOut {
 	
-	private Map<String, Product> productNameMap;
 	private Cart cart;
 
 	public CheckOut(List<Product> availableProducts) {
 		cart = new Cart(availableProducts);
-		productNameMap = availableProducts.stream().collect(Collectors.toMap(Product::getProductName, product->product));
 	}
 
 	public int price(String products) {
@@ -33,14 +28,12 @@ public class CheckOut {
 	}
 
 	private int calculatePrice(String products) {
-		Map<String, Integer> productCountMap = countProducts(products);
-		return productCountMap.keySet()
-				.stream()
-				.mapToInt(product -> productNameMap.get(product).calculatePrice(productCountMap.get(product))).sum();
+		addMultipleItemsToCart(products);
+		return cart.totalPrice();
 	}
 
-	private Map<String, Integer> countProducts(String products) {
-		return asList(products.split("")).stream().collect(toMap(product -> product, product -> 1, Integer::sum));
+	private void addMultipleItemsToCart(String products) {
+		asList(products.split("")).stream().forEach(product ->{cart.add(product);});
 	}
 
 }
