836a2d4c0b967f3dabfd9c881ab0dff183ad6b9a
unknown <XF76@INLTD09921.global.tesco.org>
2017-01-06 12:35:15 +0530

    REFACTOR - removing unused classes, move test classes to test package

--- src/main/java/org/cleancode/katas/Product.java
+++ src/main/java/org/cleancode/katas/Product.java
@@ -1,21 +1,13 @@
 package org.cleancode.katas;
 
-import java.util.List;
-
 public class Product {
 
 	private String productName;
 	private int price;
-	private List<Discount> discount;
 	
 	public Product(String productName, int price) {
 		this.productName = productName;
 		this.price = price;
-	}
-
-	public Product(String productName, int price, List<Discount> discount) {
-		this(productName,price);
-		this.discount = discount;
 	}
 	
 	public String getProductName() {
@@ -23,39 +15,7 @@ public class Product {
 	}
 		
 	public int calculatePrice(int count){
-		return isDiscountAvailable(count)?calculateDiscountedPrice(count):calculateActualPrice(count);
-	}
-	
-	private boolean isDiscountAvailable(int count){
-		return discount == null ? false:
-				discount.stream()
-				.filter(discount -> discount.isEligibleForDiscount(count))
-				.findFirst()
-				.isPresent();
-	}
-	
-	private int calculateDiscountedPrice(int count){
-		return calculateDiscountedPrice(count,availableDiscount(count));		
-	}
-	
-	private Discount availableDiscount(int count) {
-		return discount.stream()
-				.filter(discount -> discount.isEligibleForDiscount(count))
-				.findFirst()
-				.get();
-	}	
-	
-	private int calculateDiscountedPrice(int count, Discount availableDiscount){
-		return availableDiscount.calculateDiscountedPrice(count)
-				+calculateActualPrice(discountNotEligibleProductCount(count, availableDiscount));
-	}
-		
-	private int calculateActualPrice(Integer count){
 		return count*price;
 	}
-	
-	private int discountNotEligibleProductCount(int count, Discount availableDiscount) {
-		return count-availableDiscount.discountEligibleProductCount(count);
-	}
-	
+		
 }
--- src/test/java/org/cleancode/katas/ProductShould.java
+++ src/test/java/org/cleancode/katas/ProductShould.java
@@ -4,10 +4,8 @@ import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
@@ -19,14 +17,12 @@ public class ProductShould {
 	
 	private String productName;
 	private int price, numberOfProductPurchased, totalPrice;	
-	private Map<Integer, Integer> discountDetails;
 	
-	 public ProductShould(String productName, int price, int numberOfProductPurchased, int totalPrice,Map<Integer, Integer> discountDetails) {
+	 public ProductShould(String productName, int price, int numberOfProductPurchased, int totalPrice) {
 		this.productName = productName;
 		this.price = price;
 		this.numberOfProductPurchased = numberOfProductPurchased;
 		this.totalPrice = totalPrice;
-		this.discountDetails=discountDetails;
 	}
 	  
 	  @Parameters(name = "return_{3}_for_products_{0}_times{2}()")
@@ -36,30 +32,21 @@ public class ProductShould {
 		Map<Integer, Integer> discountDetailsForB = new HashMap<>();
 		discountDetailsForB.put(2, 45);
 	    return asList(new Object[][] {
-	        {"A", 50, 1, 50,discountDetails},
-	        {"A", 50, 2,100,discountDetails},	       
-	        {"A", 50, 3, 130,discountDetails},
-	        {"A", 50, 4, 180,discountDetails},
-	        {"A", 50, 6, 260,discountDetails},
-	        {"A", 50, 7, 310,discountDetails},
-	        {"B", 30, 1, 30,discountDetailsForB},
-	        {"B", 30, 2, 45,discountDetailsForB},
-	        {"B", 30, 3, 75,discountDetailsForB}
+	        {"A", 50, 1, 50},
+	        {"A", 50, 2,100},	       
+	        {"A", 50, 3, 150},
+	        {"A", 50, 4, 200},
+	        {"A", 50, 6, 300},
+	        {"A", 50, 7, 350},
+	        {"B", 30, 1, 30},
+	        {"B", 30, 2, 60},
+	        {"B", 30, 3, 90}
 	      });
 	  }
 
 	@Test
 	public void return_correct_price_for_products(){
-		List<Discount> availableDiscounts=new ArrayList<Discount>();
-		if(discountDetails != null){
-			discountDetails.keySet()
-				.stream()
-				.forEach(minimumProductCountRequired -> 
-					{
-						availableDiscounts.add(new Discount(minimumProductCountRequired, discountDetails.get(minimumProductCountRequired)));
-					});
-		}
-		Product product = new Product(productName, price,availableDiscounts);
+		Product product = new Product(productName, price);
 		assertThat(product.calculatePrice(numberOfProductPurchased), is(totalPrice));
 	}
 	
